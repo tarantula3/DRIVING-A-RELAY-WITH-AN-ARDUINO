ccd9e25de3f5d3a972978aeb86ba9c9f1364e480
tarantula3 <tarantula3@gmail.com>
2018-05-06 16:40:29 +0800

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,13 @@
+int Relay  = 2;                 // Digital pin D5
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Relay, OUTPUT);       // declare Relay as output
+}
+
+void loop() {
+  digitalWrite (Relay, HIGH);
+  delay(500);
+  digitalWrite (Relay, LOW);
+  delay(500);
+}
